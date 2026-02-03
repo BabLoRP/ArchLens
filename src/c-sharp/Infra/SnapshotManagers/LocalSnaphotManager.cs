66d073d7f67bc916c0fa5a1e8b3774f6887520e2
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2026-02-03 12:38:59 +0100

    feat: remove graph merge from `LocalSnapshotManager`

--- src/c-sharp/Infra/SnapshotManagers/LocalSnaphotManager.cs
+++ src/c-sharp/Infra/SnapshotManagers/LocalSnaphotManager.cs
@@ -2,9 +2,7 @@ using Archlens.Domain;
 using Archlens.Domain.Interfaces;
 using Archlens.Domain.Models;
 using Archlens.Domain.Models.Records;
-using System;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,29 +20,7 @@ public sealed class LocalSnaphotManager(string _localDirName, string _localFileN
         Directory.CreateDirectory(dir);
         var path = Path.Combine(dir, _localFileName);
 
-        var mergedGraph = graph;
-
-        if (File.Exists(path))
-        {
-            try
-            {
-                var existingJson = await File.ReadAllTextAsync(path, ct);
-                var existingGraph = DependencyGraphSerializer.Deserialize(existingJson, root);
-
-                if (existingGraph is DependencyGraphNode existingRoot &&
-                    graph is DependencyGraphNode newRoot)
-                {
-                    MergeGraphs(existingRoot, newRoot);
-                    mergedGraph = existingRoot;
-                }
-            }
-            catch
-            {
-                mergedGraph = graph;
-            }
-        }
-
-        var json = DependencyGraphSerializer.Serialize(mergedGraph);
+        var json = DependencyGraphSerializer.Serialize(graph);
         await File.WriteAllTextAsync(path, json, ct);
     }
 
@@ -63,35 +39,5 @@ public sealed class LocalSnaphotManager(string _localDirName, string _localFileN
         var graph = DependencyGraphSerializer.Deserialize(json, root);
 
         return graph ?? null;
-    }
-
-    private static void MergeGraphs(DependencyGraphNode existingRoot, DependencyGraphNode newRoot)
-    {
-        foreach (var newChild in newRoot.GetChildren())
-            UpsertChild(existingRoot, newChild);
-    }
-
-    private static void UpsertChild(DependencyGraphNode parent, DependencyGraph newChild)
-    {
-        var existing = parent.GetChildren()
-            .FirstOrDefault(c => string.Equals(c.Path, newChild.Path, StringComparison.OrdinalIgnoreCase));
-
-        if (existing is null)
-        {
-            parent.AddChild(newChild);
-            return;
-        }
-
-        if (existing is DependencyGraphNode existingNode && newChild is DependencyGraphNode newNode)
-        {
-            existingNode.ReplaceDependencies(newNode.GetDependencies());
-
-            foreach (var grandChild in newNode.GetChildren())
-                UpsertChild(existingNode, grandChild);
-
-            return;
-        }
-
-        parent.ReplaceChild(newChild);
     }
 }
