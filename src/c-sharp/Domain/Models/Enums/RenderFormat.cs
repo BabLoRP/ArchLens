f4beccc6c1610a8a2ffbcf18de8337e74cd1bda9
Lotte <lottejd@gmail.com>
2026-02-04 14:23:21 +0100

    Use expected filename and better conversion to file extension

--- src/c-sharp/Domain/Models/Enums/RenderFormat.cs
+++ src/c-sharp/Domain/Models/Enums/RenderFormat.cs
@@ -5,3 +5,16 @@ public enum RenderFormat
     Json,
     PlantUML
 }
+
+public static class RenderFormatExtensions
+{
+    public static string ToFileExtension(this RenderFormat format)
+    {
+        return format switch
+        {
+            RenderFormat.Json => "json",
+            RenderFormat.PlantUML => "puml",
+            _ => format.ToString(),
+        };
+    }
+}
