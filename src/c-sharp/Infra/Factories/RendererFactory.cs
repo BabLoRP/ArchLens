9c2209189d1576a9474935ad65523d380c2c228f
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2026-02-05 11:26:01 +0100

    refactor: correct options type for RendererFactory

--- src/c-sharp/Infra/Factories/RendererFactory.cs
+++ src/c-sharp/Infra/Factories/RendererFactory.cs
@@ -1,16 +1,17 @@
-using System;
 using Archlens.Domain.Interfaces;
 using Archlens.Domain.Models.Enums;
+using Archlens.Domain.Models.Records;
 using Archlens.Infra.Renderers;
+using System;
 
 namespace Archlens.Infra.Factories;
 
 public sealed class RendererFactory
 {
-    public static IRenderer SelectRenderer(RenderFormat f) => f switch
+    public static IRenderer SelectRenderer(RenderOptions options) => options.Format switch
     {
         RenderFormat.Json => new JsonRenderer(),
         RenderFormat.PlantUML => new PlantUMLRenderer(),
-        _ => throw new ArgumentOutOfRangeException(nameof(f))
+        _ => throw new ArgumentOutOfRangeException(nameof(options))
     };
 }
