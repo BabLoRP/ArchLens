7902729846c7fbd8c332c7feeaad913e23d7d72b
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2026-02-13 10:41:42 +0100

    feat: add record `ProjectChanges` that includes deletions

---
+++ src/c-sharp/Domain/Models/Records/ProjectChanges.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections.Generic;
+
+namespace Archlens.Domain.Models.Records;
+
+public sealed record ProjectChanges(
+    IReadOnlyDictionary<string, IReadOnlyList<string>> ChangedFilesByDirectory,
+    IReadOnlyList<string> DeletedFiles,
+    IReadOnlyList<string> DeletedDirectories
+);
