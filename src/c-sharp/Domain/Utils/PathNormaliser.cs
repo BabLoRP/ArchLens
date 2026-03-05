341e3bd62fab944a1da4183900c60a305fcaafa7
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2026-03-05 19:07:02 +0100

    refactor: moved relative path to models

--- src/c-sharp/Domain/Utils/PathNormaliser.cs
+++ src/c-sharp/Domain/Utils/PathNormaliser.cs
@@ -2,22 +2,6 @@
 
 namespace Archlens.Domain.Utils;
 
-public readonly record struct RelativePath
-{
-    public string Value { get; }
-
-    private RelativePath(string value) => Value = value;
-
-    public static RelativePath File(string projectRoot, string input)
-        => new(PathNormaliser.NormaliseFile(projectRoot, input));
-
-    public static RelativePath Directory(string projectRoot, string input)
-        => new(PathNormaliser.NormaliseModule(projectRoot, input));
-
-    public override string ToString() => Value;
-}
-
-
 public static class PathNormaliser
 {
     public const string RelativeRoot = "./";
