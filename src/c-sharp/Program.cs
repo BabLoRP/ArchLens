f29491ebaf885f1e7295b958c1c2e9ec31213ea8
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2026-03-07 08:59:37 +0100

    fix: diff args should be trimmed

--- src/c-sharp/Program.cs
+++ src/c-sharp/Program.cs
@@ -14,7 +14,7 @@ public class Program
     {
         var path = args.Length == 0 ? string.Empty : args[0].Trim();
         var format = args.Length < 2 ? "puml" : args[1].Trim();
-        var diff = args.Length < 3 ? false : args[2] == "diff";
+        var diff = args.Length < 3 ? false : args[2].Trim() == "diff";
 
         await CLI(path, format, diff);
     }
